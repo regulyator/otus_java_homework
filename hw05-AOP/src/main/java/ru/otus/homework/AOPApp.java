53b62377a4ecdde341fd8d9f431e14e185cb1b2f
regulyator <regulyator777@gmail.com>
2020-08-29 02:48:49 +1000

    +исправления по замечаниям, теперь банкомат может непосредственно загружать купюры, убрал лишние прослойки

--- hw05-AOP/src/main/java/ru/otus/homework/AOPApp.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.homework;
-
-public class AOPApp {
-
-    public static void main(String... args) {
-        SomeBoringClass someBoringClass = new SomeBoringClass();
-        someBoringClass.saySomething("What Does The Fox Say?!");
-    }
-}
