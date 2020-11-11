646f4c922518a47b0c7d5a682cb9d6051551ab2c
regulyator <regulyator777@gmail.com>
2020-11-11 23:54:27 +1000

    finish HM

---
+++ hm11-mycache/src/main/java/ru/otus/hibernate/dao/UserDaoHibernate.java
@@ -0,0 +1,74 @@
+package ru.otus.hibernate.dao;
+
+import org.hibernate.Session;
+import ru.otus.core.dao.Dao;
+import ru.otus.core.model.User;
+import ru.otus.core.sessionmanager.SessionManager;
+import ru.otus.hibernate.exceptions.HibernateDaoException;
+import ru.otus.hibernate.sessionmanager.DatabaseSessionHibernate;
+import ru.otus.hibernate.sessionmanager.SessionManagerHibernate;
+
+import java.util.Optional;
+
+public class UserDaoHibernate implements Dao<User, Long> {
+
+    private final SessionManagerHibernate sessionManager;
+
+    public UserDaoHibernate(SessionManagerHibernate sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        try {
+            return Optional.ofNullable(getSession().getHibernateSession().find(User.class, id));
+        } catch (Exception ex) {
+            throw new HibernateDaoException("Error when try findById!", ex);
+        }
+    }
+
+    @Override
+    public Long insert(User user) {
+        try {
+            Session hibernateSession = getSession().getHibernateSession();
+            hibernateSession.persist(user);
+            hibernateSession.flush();
+            return user.getId();
+        } catch (Exception ex) {
+            throw new HibernateDaoException("Error when try insert user!", ex);
+        }
+    }
+
+    @Override
+    public void update(User user) {
+        try {
+            Session hibernateSession = getSession().getHibernateSession();
+            hibernateSession.merge(user);
+            hibernateSession.flush();
+        } catch (Exception ex) {
+            throw new HibernateDaoException("Error when try update user!", ex);
+        }
+    }
+
+    @Override
+    public void insertOrUpdate(User user) {
+        try {
+            Session hibernateSession = getSession().getHibernateSession();
+            hibernateSession.saveOrUpdate(user);
+            hibernateSession.flush();
+        } catch (Exception ex) {
+            throw new HibernateDaoException("Error when try insertOrUpdate user!", ex);
+        }
+    }
+
+    @Override
+    public SessionManager getSessionManager() {
+        return this.sessionManager;
+    }
+
+    private DatabaseSessionHibernate getSession() {
+        return sessionManager.getCurrentSession();
+    }
+
+
+}
